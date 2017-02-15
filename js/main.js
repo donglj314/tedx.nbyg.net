3bd711c93c12b33e41851b95cb003db37816333b
Jason Dong <donglj314@126.com>
2017-02-15 17:38:55 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function load() {
-    document.getElementsByClassName('img-blooming')[0].id = "";
+    document.getElementsByClassName('img-everything')[0].id = "";
     document.getElementsByClassName('hero-logo')[0].id = "";
     document.getElementsByClassName('hero-right')[0].id = "";
     document.getElementsByClassName('site-nav')[0].id = "";
